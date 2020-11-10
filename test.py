c69fa70f210d806b56a0146f852936468d8216b2
Aideen McLoughlin <37660493+a-mcloughlin@users.noreply.github.com>
2020-11-10 21:57:23 +0000

    Add files to pass tests

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from unittest import TestLoader, TextTestRunner, TestSuite
 from test.test_sentiment import TestSentiment
 from test.test_twitter import TestTwitter
 from test.test_word_processing import TestWordProcessing
+import nltk
 
 if __name__ == '__main__':
+    nltk.download('stopwords')
     
     loader = TestLoader()
     tests = [
@@ -14,4 +16,3 @@ if __name__ == '__main__':
 
     runner = TextTestRunner(verbosity=2)
     runner.run(suite)
-
