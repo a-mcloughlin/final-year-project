a6fb6fa5bc4ae246bb06d48f9244887f1af4f912
Aideen McLoughlin <aideen.mcloughlin@gmail.com>
2021-02-02 21:36:52 +0000

    First Draft of GUI with Analyse and Compare Tabs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from analyse import analyse
-resultlist = []
+import os
+resultlist = [None,None]
+resultitem = None
 app = Flask(__name__)
 
 # Add a result to the list of results 
@@ -13,16 +15,44 @@ def add_to_resultlist(resultitem, resultlist):
 # If nothing has been passed, display an empty html page
 @app.route("/")
 def hello():              
-    return render_template('index.html', resultlist=resultlist)
+    return render_template('tabs/analyse.html', result=resultitem)
+
+@app.route('/analyse')
+def search():
+    resultlist[0] = None
+    resultlist[1] = None
+    return render_template('tabs/analyse.html', result=resultitem)
+
+@app.route('/compare')
+def compare():
+    resultitem = None
+    return render_template('tabs/compare.html', resultlist=resultlist)
 
 # If a request has been made, render the results on the page
-@app.route('/query', methods=['POST'])
-def queryPage():
+@app.route('/analyse', methods=['POST'])
+def analyseQuery():
     term = request.form['twitter_query']
-    resultitem = analyse(term)    
-    add_to_resultlist( resultitem, resultlist)
-    return render_template('index.html', resultlist=resultlist)   
+    resultitem = analyse(term)
+    return render_template('tabs/analyse.html', result=resultitem)   
+
+
+# If a request has been made, render the results on the page
+@app.route('/compare', methods=['POST'])
+def compareQuery():
+    term = request.form.get('twitter_query', None)
+    lp = request.form.get('loopnum', 0)
+    result = analyse(term)    
+    if lp == '1':
+        resultlist[0] = result
+    elif lp == '2':
+        resultlist[1] = result
+    return render_template('tabs/compare.html', resultlist=resultlist)   
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # This webapp runs on port 8081
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=8081)
+
